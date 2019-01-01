d2cd1fd9cf005f4096a3e3ab58f886475d7ed645
solemcaelum <tripyvox@gmail.com>
2019-01-01 13:57:24 -0600

    Added Discord Bot functions.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-  return string.replace(/\s/g, "");
-};
+var exports = module.exports = {};
+const {Client, Attachment} = require('discord.js');
+var token = "";
+
+exports.setToken = function(data) {
+  token = data;
+}
+
+exports.startBot = function{
+  const client = new Client();
+
+  client.on('ready', () => {
+          console.log('[CLIENT] Ready!');
+          client.user.setActivity("packets", { type: "STREAMING", url: "https://www.twitch.tv/fillyanon" })
+  });
+
+  client.login(token);
+}
